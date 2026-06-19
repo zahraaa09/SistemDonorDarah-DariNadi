f1c103437eb5e786bbcfe9553166428c833a0bb8
nabilasalsabilaaa <naabilasalsabiilaa@gmail.com>
2026-06-19 20:24:37 +0800

    del all

--- src/main/java/com/donordarah/donordarurat/repository/locationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.donordarah.donordarurat.repository;
-
-import com.donordarah.donordarurat.model.location;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface locationRepository extends JpaRepository<location, Integer> {
-}
